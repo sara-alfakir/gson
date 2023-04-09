255db12671ed02643a5843d6f52c6772a7c43548
sara-alfakir <sara2001fa@gmail.com>
2023-04-10 01:00:55 +0200

    supprimer deux duplications de code dans JsonParser

--- gson/src/main/java/com/google/gson/JsonParser.java
+++ gson/src/main/java/com/google/gson/JsonParser.java
@@ -73,12 +73,10 @@ public final class JsonParser {
         throw new JsonSyntaxException("Did not consume the entire document.");
       }
       return element;
-    } catch (MalformedJsonException e) {
+    } catch (MalformedJsonException | NumberFormatException e) {
       throw new JsonSyntaxException(e);
     } catch (IOException e) {
       throw new JsonIOException(e);
-    } catch (NumberFormatException e) {
-      throw new JsonSyntaxException(e);
     }
   }
 
@@ -101,9 +99,7 @@ public final class JsonParser {
     reader.setLenient(true);
     try {
       return Streams.parse(reader);
-    } catch (StackOverflowError e) {
-      throw new JsonParseException("Failed parsing JSON source: " + reader + " to Json", e);
-    } catch (OutOfMemoryError e) {
+    } catch (StackOverflowError |OutOfMemoryError  e) {
       throw new JsonParseException("Failed parsing JSON source: " + reader + " to Json", e);
     } finally {
       reader.setLenient(lenient);
